14c36d5b691a8361d7af2286b91dff8e09da269f
allensarmiento <allensarmiento3@gmail.com>
2019-06-29 00:19:06 -0700

    Implemented and reading from a database for the shop

--- app.js
+++ app.js
@@ -1,53 +1,57 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+    app = express(),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser");
 
-app.use(express.static("public")); // Public folder for css styles
+mongoose.connect("mongodb://localhost:27017/Coffee_Shop",  {useNewUrlParser: true});
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); // Expect ejs files
+app.use(express.static("public")); // Public folder for css styles
 
-app.get("/", function(req, res) {
-  res.render("home");
+// NOTE: Mongoose by default add an S, so to prevent looking for Shops, specify to look for Shop as the 3rd parameter.
+var shopSchema = new mongoose.Schema({
+  blend: String,
+  name: String,
+  price: String,
+  image: String
 });
+var Shop = mongoose.model("Shop", shopSchema, "Shop"); 
 
-// ============ TODO: Data for Shop ===============
-var items = [
-  {blend: "Dark", name: "Dark Expresso", price: "$0.00", image: "https://cdn.shopify.com/s/files/1/0002/1115/7052/products/brazil1_dkroast_400.jpg?v=1549596309"},
-  {blend: "Dark", name: "Haitian", price: "$0.00", image: "https://www.holybeanscafe.com/holybeanscafe/wp-content/uploads/2015/11/dark-roast-coffee.jpg"},
-  {blend: "Dark", name: "Paris", price: "0.00", image: "http://cdn.shopify.com/s/files/1/2217/5179/products/frc-french-roast-roasted-coffee-beans_grande.jpg?v=1505296912"},
-  {blend: "Dark", name: "Mocha Java", price: "0.00", image: "https://sep.yimg.com/ay/yhst-133788258843600/mocha-java-organic-coffee-beans-41.gif"},
-  
-  {blend: "Medium", name: "Medium Expresso", price: "0.00", image: "https://www.coffeebeandirect.com/media/catalog/product/cache/1/image/0dc6da5b2fe15547a809e30525508fd7/m/e/medium-roast-espresso-wide.jpg"},
-  {blend: "Medium", name: "Coffee Club", price: "0.00", image: "https://static1.squarespace.com/static/59d9113c90bade192af3e3a0/t/5a2dc13b53450ab787d59f99/1512948028366/food-beans-coffee-drink.jpg"},
-  {blend: "Medium", name: "Colombia", price: "0.00", image: "http://www.topratedcoffeebeans.com/wp-content/uploads/2014/11/Vienna-roast-beans-300x300.png"},
-  
-  {blend: "Light", name: "", price: "", image: ""},
-  {blend: "", name: "", price: "", image: ""},
-];
+var storeSchema = new mongoose.Schema({
+  city: String,
+  street: String, 
+  bgImage: String
+});
+var Stores = mongoose.model("Stores", storeSchema, "Stores");
+
+// ============
+//    ROUTES
+// ============
+app.get("/", function(req, res) {
+  res.render("home", {page: "home"});
+});
 
 app.get("/shop", function(req, res) {
-  res.render("shop");
+  Shop.find({}, function(err, items) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("shop", {items: items});
+    }
+  })
 });
-// ================================================
-
-// ============= Stores ===========================
-var stores = [
-  {city: "San Francisco", street: "Golden Gate", bgImage: "https://images.unsplash.com/photo-1487132906645-8e0fbba067e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"},
-  {city: "San Francisco", street: "Mission District", bgImage: "https://images.unsplash.com/photo-1439396087961-98bc12c21176?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1780&q=80"},
-  {city: "San Francisco", street: "Midtown Terrace", bgImage: "https://images.unsplash.com/photo-1445359179985-460648949e10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80"},
-  {city: "San Francisco", street: "University Mound", bgImage: "https://images.unsplash.com/photo-1458169495136-854e4c39548a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-  
-  {city: "Los Angeles", street: "Chinatown", bgImage: "https://images.unsplash.com/photo-1521123493239-e0f9ae0445ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"},
-  {city: "Los Angeles", street: "Fashion District", bgImage: "https://images.unsplash.com/photo-1557882518-693670985a99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80"},
-  {city: "Los Angeles", street: "Rampart Village", bgImage: "https://images.unsplash.com/photo-1471039497385-b6d6ba609f9c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"},
-
-  {city: "New York", street: "East Village", bgImage: "https://images.unsplash.com/photo-1512486046-0d05000458ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80"},
-  {city: "New York", street: "Brooklyn Heights", bgImage: "https://images.unsplash.com/photo-1484861671664-4ebd42ced711?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-  {city: "New York", street: "Greenpoint", bgImage: "https://images.unsplash.com/photo-1450705354661-932f7b21aa51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"},
-];
 
 app.get("/stores", function(req, res) {
-  res.render("stores", {stores:  stores});
+  Stores.find({}, function(err, stores) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("stores", {stores: stores});
+    }
+  })
 });
-// ================================================
 
 app.get("/story", function(req, res) {
   res.render("story");
