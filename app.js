cd511055563a224fb68fc24728a3e5f8f0cae27d
allensarmiento <allensarmiento3@gmail.com>
2019-07-12 13:54:10 -0700

    Update username logged in the navbar

--- app.js
+++ app.js
@@ -16,20 +16,27 @@ mongoose.connect(url,  {useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs"); // Expect ejs files
+app.use(express.static(__dirname + "/public")); // Public folder for css styles
+
+// PASSPORT CONFIGURATION
 app.use(require("express-session")({
   secret: "This is a default sentence",
   resave: false,
   saveUninitialized: false
 }));
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs"); // Expect ejs files
-app.use(express.static(__dirname + "/public")); // Public folder for css styles
 app.use(passport.initialize());
 app.use(passport.session());
-
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// Middleware to be used on all pages to tell if user is logged in or not.
+app.use(function(req, res, next) {
+  res.locals.currentUser =  req.user;
+  next();
+});
 
 // ============
 //    ROUTES
