a4f6c50e34ac72c80dd25024ef70f4d1d91a78c7
allensarmiento <allensarmiento3@gmail.com>
2019-07-19 14:53:51 -0700

    Update cart information and number of items in cart

--- public/js/addItem.js
+++ public/js/addItem.js
@@ -1,8 +1,47 @@
-function addToCart(itemName) {
-  let cartNumber = document.getElementById("cart-number");
-  if (cartNumber.textContent) {
-    cartNumber.textContent++;
+// Add item to cart
+function addToCart(itemName, itemImage) {
+  if (sessionStorage.getItem(itemName)) {
+    // If item already in session storage
+    let value = JSON.parse(sessionStorage[itemName]);
+    value.quantity = Number(value.quantity + 1);
+    sessionStorage[itemName] = JSON.stringify(value);
   } else {
-    cartNumber.textContent = 1;
+    // Item not in session storage
+    let value = {
+      image: itemImage,
+      quantity: 1
+    };
+    sessionStorage.setItem(itemName, JSON.stringify(value)); // key: item name, value: quantity
+  }
+  // Update cart number
+  updateCartItems();
+}
+
+// Update the number of cart items
+function updateCartItems() {
+  // Get place to show the number of items in cart
+  let cartNumber = document.getElementById("cart-number");
+  let count = 0;
+  // Loop through items in cart and count the quantity
+  for (let i=0, len=sessionStorage.length; i<len; i++) {
+    let key = sessionStorage.key(i);
+    let value = JSON.parse(sessionStorage[key]);
+    quantity = Number(value.quantity);
+    count += quantity;
+    // Update cart item number if greater than 0
+    if (Number(count) > 0) {
+      cartNumber.textContent = Number(count);
+    }
+  }
+}
+
+function clearCart() {
+  sessionStorage.clear();
+}
+
+function printSessionStorage() {
+  for (let i =0, len=sessionStorage.length; i < len; i++) {
+    let key = sessionStorage.key(i);
+    console.log(key);
   }
 }
